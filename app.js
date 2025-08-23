8419c2f487b0958c4781db761919fc202d91c4b5
techwebdots <techwebdots@gmail.com>
2025-08-23 13:05:50 +0530

    special symbol

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World by TechWebDots :) \n');
+  res.end('Hello World by TechWebDots 🎉:) \n');
 });
 
 server.listen(port, hostname, () => {
